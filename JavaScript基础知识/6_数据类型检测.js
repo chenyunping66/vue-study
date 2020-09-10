145549a4195a75f6ed14b12bb9eab54283a7f063
1111 <1276830496@qq.com>
2020-09-10 20:44:56 +0800

    今日学习

--- JavaScript基础知识/6_数据类型检测.js
+++ JavaScript基础知识/6_数据类型检测.js
@@ -2,7 +2,19 @@
 * 基于typeof检测出来的结果
 *   1.首先是一个字符串
 *  2.字符串包含对应的类型
+*
+*局限性：
+*   1. typeof null => "object" 但是null并不是对象
+*   2. 基于typeof无法细分出当前值是普通对象还是数组对象等数据类型，返回的结果是“object”
 */
 console.log(typeof 1); //"number"  返回的结果是字符串number
 let a = NaN;
 console.log(typeof a);    ///控制台输出的蓝色是是字符串，黑色是数字
+
+//面试题
+console.log(typeof typeof typeof []); //"object"
+// 第一步先算这个 typeof [] => "object"
+// 第二步算这个 => typeof "object" => "string"
+//因为typeof检测的结果是字符串（ "string"），所以只要两个及以上同时检测，最后结果必然是“string”
+
+
